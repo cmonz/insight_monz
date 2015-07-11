09c5bed8f94b81f66e614e90116ebf6187905679
cmonz <cesarsmonzon@gmail.com>
2015-07-11 00:52:21 -0500

    median

---
+++ src/median_unique.py
@@ -0,0 +1,49 @@
+import tempfile
+import itertools
+import heapq
+
+LINES_HOLD_IN_MEM = 1000000
+FILE_OUT = '/tweet_output/ft2.txt'
+FILE_IN = '/tweet_input/tweets.txt'
+MEDIAN_LIST = []
+
+def get_batch_it(file_name = FILE_IN):
+    file_handle = open(file_name, 'rb')
+    it = itertools.groupby(file_handle, key=lambda k,
+                           line=itertools.count(): next(line) // LINES_HOLD_IN_MEM)
+    return it, file_handle
+
+def close_file(file_handle):
+    return file_handle.close()
+
+def count_unique(line):
+    return len(set(line[:-1].split(' ')))
+
+def write_median(batch_it):
+    with open(FILE_OUT, 'w') as fout:
+        for k, group in batch_it:
+            for line in group:
+                MEDIAN_LIST.append(count_unique(line))
+                median_len = len(MEDIAN_LIST)
+                result = 0
+                if median_len % 2 == 0:
+                    result = (MEDIAN_LIST[median_len // 2] + MEDIAN_LIST[(median_len // 2) - 1]) / 2.0
+                else:
+                    result = MEDIAN_LIST[median_len // 2]
+                fout.write(str(result) + '\n')
+        
+def main():
+
+    batch_it, raw_file_handle = get_batch_it()
+    write_median(batch_it)
+    close_file(raw_file_handle)
+
+    print 'Median calculated in batches and output to ', FILE_OUT
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ src/words_tweeted.py
@@ -0,0 +1,90 @@
+import tempfile
+import itertools
+import heapq
+
+LINES_HOLD_IN_MEM = 1000000
+FILE_OUT = '/tweet_output/ft1.txt'
+FILE_IN = '/tweet_input/tweets.txt'
+
+
+def get_batch_it(file_name = FILE_IN):
+    file_handle = open(file_name, 'rb')
+    it = itertools.groupby(file_handle, key=lambda k,
+                           line=itertools.count(): next(line) // LINES_HOLD_IN_MEM)
+    return it, file_handle
+
+def close_file(file_handle):
+    return file_handle.close()
+
+
+def merge_write(file_name, tempFiles):
+    with open(file_name, 'w') as dest:
+    
+        merged = heapq.merge(*tempFiles)
+        past_item = merged.next()
+        counter = 1
+        
+        for item in merged:
+            if item != past_item:
+                dest.write(past_item[:-1] + '\t' + str(counter) + '\n')
+                past_item = item
+                counter = 1
+            else:
+                counter += 1 
+                
+        dest.write(past_item[:-1] + '\t' + str(counter) + '\n')
+        
+
+def close_files(file_list):
+    for each in file_list:
+        each.close()
+    
+def write_sorted_batches(batch_it, temp_list):
+    for k, group in batch_it:
+        sorted_chunk = sorted((''.join(group)).replace('\r\n', ' ').split(' '))[1:]
+        temp = tempfile.TemporaryFile()
+        for item in sorted_chunk:
+            temp.write(item + '\n')
+        temp_list.append(temp)
+        del sorted_chunk
+        temp.seek(0)
+
+    return temp_list
+
+
+        
+##        a = time.time()
+##
+##        
+##        for m in group:
+##            median.append(countUnique(m))
+##
+##            f = 0
+##            if len(median) % 2 == 0:
+##                f = (median[len(median) // 2] + median[(len(median) // 2) - 1]) / 2.0
+##            else:
+##                f = median[(len(median) // 2 )]
+##
+##        print time.time() - a
+
+
+        
+
+def main():
+    
+    batch_it, raw_file_handle = get_batch_it()
+    temp_files = []
+    temp_files = write_sorted_batches(batch_it, temp_files)
+    merge_write(FILE_OUT, temp_files)
+    close_files(temp_files)
+    close_file(raw_file_handle)
+
+    print 'Tweets sorted and output to ', FILE_OUT
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
